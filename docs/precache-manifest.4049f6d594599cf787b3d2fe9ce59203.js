a7368eeab44ac24bdc7e5dfd14e557e20acb3255
Filipe Merker <filipesmerker@gmail.com>
2021-02-01 19:08:53 +0100

    Add build to repo

---
+++ docs/precache-manifest.4049f6d594599cf787b3d2fe9ce59203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd478bf04a0f939c5db",
+    "url": "/css/app.64b6f3be.css"
+  },
+  {
+    "revision": "ce387a315dd4719e3f764c700986013e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffd478bf04a0f939c5db",
+    "url": "/js/app.3028be07.js"
+  },
+  {
+    "revision": "8335494cb23efa3bb3fb",
+    "url": "/js/chunk-2d0c1642.e8e87d3e.js"
+  },
+  {
+    "revision": "9fd872ea53944655fa4d",
+    "url": "/js/chunk-vendors.127aa1e4.js"
+  },
+  {
+    "revision": "b400fd73d650f2a6a929915212958bf9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
