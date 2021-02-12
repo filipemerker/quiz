fede3b8bac261451c817a10ea05388b56bba4b1f
Filipe Merker <filipesmerker@gmail.com>
2021-02-12 21:17:39 +0100

    Redesign challenge page

--- src/components/MultipleAnswer/Question.vue
+++ src/components/MultipleAnswer/Question.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="flex flex-col justify-between">
     <div class="w-full flex-grow flex flex-col justify-center">
-      <div
-        class="fold text-left rounded-md gradient-1 shadow-lg flex flex-wrap flex-col py-8 px-3 mx-4 mb-6"
-      >
+      <div class=" text-left  flex flex-wrap flex-col py-8 mx-5 mb-6">
         <span
           v-if="context"
           data-testid="question-title-before"
-          class="select-none text-md mb-3 opacity-80 text-white w-full"
+          class="select-none text-lg mb-3 opacity-80 text-white w-full"
         >
           {{ question.title[0] }}
         </span>
@@ -21,7 +19,7 @@
         <span
           v-if="context"
           data-testid="question-title-after"
-          class="select-none text-md mt-4 opacity-80 text-white w-full"
+          class="select-none text-lg mt-4 opacity-80 text-white w-full"
         >
           {{ question.title[2] }}
         </span>
@@ -29,13 +27,13 @@
     </div>
     <div
       id="alternatives"
-      class=" bg-gray-200 shadow-inner w-full p-5 flex flex-wrap content-center justify-between sm:rounded-md sm:mb-5"
+      class="   p-5 flex flex-wrap content-center justify-between rounded-md"
     >
       <button
         v-for="alternative in question.alternatives"
         :key="alternative"
         :class="
-          `alternative my-2 h-12 shadow-md font-bold text-xs text-gray-800 bg-gray-300 border-gray-400 border-b-2 rounded-md px-2`
+          `alternative my-2 h-12 shadow-lg font-semibold text-sm text-gray-100 bg-gray-100 bg-opacity-30 rounded-md px-2`
         "
         :data-testid="alternative"
         @click="onSelect(alternative, question.rightAlternative)"
